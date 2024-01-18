c91606cd3fba7518264316f6032700b339cb1fd4
DamianoL <damianolunardo@hotmail.it>
2024-01-18 09:39:31 +0100

    Add - category controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,5 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/projects', [ProjectController::class, 'index']);
 Route::get('/projects/{project:slug}', [ProjectController::class, 'show']);
-
+Route::get('/categories/{type:slug}', [CategoryController::class, 'show']);
 
